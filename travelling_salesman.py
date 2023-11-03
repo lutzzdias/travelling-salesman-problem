d1a723657852851899897a95a6ae1515557d33cb
lutzzdias <thiagolutzdias@hotmail.com>
2023-11-03 22:16:39 +0000

    feat: setup general API for implementation

---
+++ travelling_salesman.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2023 Alexandre Jesus <https://adbjesus.com>, Carlos M. Fonseca <cmfonsec@dei.uc.pt>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+from typing import TextIO, Optional, Any
+from collections.abc import Iterable, Hashable
+
+Objective = Any
+
+class Component:
+    @property
+    def cid(self) -> Hashable:
+        raise NotImplementedError
+
+
+class LocalMove:
+    ...
+
+
+class Solution:
+    def output(self) -> str:
+        """
+        Generate the output string for this solution
+        """
+        raise NotImplementedError
+
+
+    def copy(self) -> Solution:
+        """
+        Return a copy of this solution.
+
+        Note: changes to the copy must not affect the original
+        solution. However, this does not need to be a deepcopy.
+        """
+        raise NotImplementedError
+
+
+    def is_feasible(self) -> bool:
+        """
+        Return whether the solution is feasible or not
+        """
+        raise NotImplementedError
+
+
+    def objective(self) -> Optional[Objective]:
+        """
+        Return the objective value for this solution if defined, otherwise
+        should return None
+        """
+        raise NotImplementedError
+
+
+    def lower_bound(self) -> Optional[Objective]:
+        """
+        Return the lower bound value for this solution if defined,
+        otherwise return None
+        """
+        raise NotImplementedError
+
+
+    def add_moves(self) -> Iterable[Component]:
+        """
+        Return an iterable (generator, iterator, or iterable object)
+        over all components that can be added to the solution
+        """
+        raise NotImplementedError
+
+
+    def local_moves(self) -> Iterable[LocalMove]:
+        """
+        Return an iterable (generator, iterator, or iterable object)
+        over all local moves that can be applied to the solution
+        """
+        raise NotImplementedError
+
+
+    def random_local_move(self) -> Optional[LocalMove]:
+        """
+        Return a random local move that can be applied to the solution.
+
+        Note: repeated calls to this method may return the same
+        local move.
+        """
+        raise NotImplementedError
+
+
+    def random_local_moves_wor(self) -> Iterable[LocalMove]:
+        """
+        Return an iterable (generator, iterator, or iterable object)
+        over all local moves (in random order) that can be applied to
+        the solution.
+        """
+        raise NotImplementedError
+
+            
+    def heuristic_add_move(self) -> Optional[Component]:
+        """
+        Return the next component to be added based on some heuristic
+        rule.
+        """
+        raise NotImplementedError
+
+
+    def add(self, component: Component) -> None:
+        """
+        Add a component to the solution.
+
+        Note: this invalidates any previously generated components and
+        local moves.
+        """
+        raise NotImplementedError
+
+
+    def step(self, lmove: LocalMove) -> None:
+        """
+        Apply a local move to the solution.
+
+        Note: this invalidates any previously generated components and
+        local moves.
+        """
+        raise NotImplementedError
+
+
+    def objective_incr_local(self, lmove: LocalMove) -> Optional[Objective]:
+        """
+        Return the objective value increment resulting from applying a
+        local move. If the objective value is not defined after
+        applying the local move return None.
+        """
+        raise NotImplementedError
+
+
+    def objective_incr_add(self, component: Component) -> Optional[Objective]:
+        """
+        Return the objective value increment resulting from adding a
+        component. If the objective value is not defined after adding the
+        component, return None.
+        """
+        raise NotImplementedError
+
+
+    def lower_bound_incr_add(self, component: Component) -> Optional[Objective]:
+        """
+        Return the lower bound increment resulting from adding a
+        component. If the lower bound is not defined after adding the
+        component, return None.
+        """
+        raise NotImplementedError
+
+
+    def perturb(self, ks: int) -> None:
+        """
+        Perturb the solution in place. The amount of perturbation is
+        controlled by the parameter ks (kick strength)
+        """
+        raise NotImplementedError
+
+
+    def components(self) -> Iterable[Component]:
+        """
+        Returns an iterable to the components of a solution
+        """
+        raise NotImplementedError
+
+
+class Problem:
+    def __init__(self, dimension, distance_matrix):
+        self.dimension = dimension              # Number of cities
+        self.distance_matrix = distance_matrix  # Distance matrix where distance_matrix[i][j] is the distance between city i and city j
+        
+
+    @classmethod
+    def from_textio(cls, f: TextIO) -> Problem:
+        """
+        Create a problem from a text I/O source `f`
+        """
+        raise NotImplementedError
+
+
+    def empty_solution(self) -> Solution:
+        """
+        Create an empty solution (i.e. with no components).
+        """
+        raise NotImplementedError
